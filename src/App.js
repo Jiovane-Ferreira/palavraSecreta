112c4daebf73a087f3dc5da935c774df951b2b98
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2024-04-23 16:58:59 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-
+// CSS
 import './App.css';
 
+// Data
+import { wordlist } from './data/data';
+
+//React
+import { useState, useCallback, useEffect } from 'react';
+
+// Components
+import StartScreen from './components/StartScreen';
+
+const stage = [
+  {id: 1, name: "start"},
+  {id: 2, name: "game"},
+  {id: 3, name: "end"},
+];
+
+
 function App() {
+
+  const [gameStage, setGameStage] = useState(stage[0].name);
+
   return (
     <div className="App">
-      <h2>Secret Word</h2>
+
+      {gameStage === 'start' && <StartScreen />};
+      {gameStage === 'game' && <StartScreen />};
+      {gameStage === 'end' && <StartScreen />};
+            
     </div>
   );
 }
---
+++ src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+    return (
+        <div className="start_container">
+            <h1>Palavra Secreta</h1>
+            <p>Clique no botão abaixo para começar a jogar</p>
+            <button>Começar Jogo</button>
+        </div>
+    )
+}
+
+export default StartScreen;
