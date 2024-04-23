5f18fd7e52c68bb3837f8c30d7615594b5aa4778
Jiovane Ferreira <jiovane.ferreira@gmail.com>
2024-04-23 16:32:41 -0300

    criação dos principais diretórios

---
+++ src/data/data.js
@@ -0,0 +1,10 @@
+export const wordlist = {
+    
+    carro: ["Motor", "Porta", "Capo", "Rodas", "Antena"],
+    fruta: ["Maça", "Pera", "Mamao", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    programação: ["Linguagem", "Framework", "Javascript", "React"],
+    alimento: ["Arroz", "Feijão", "Carne", "Ovo", "Salada"],
+
+};
